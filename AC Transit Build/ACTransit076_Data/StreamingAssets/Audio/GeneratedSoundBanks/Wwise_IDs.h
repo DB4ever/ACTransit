baa92f5421ac7fc612843c92a62d9e5f6ae55754
Daniel B <Breyd28@gmail.c>
2018-04-15 03:53:02 -0700

    ACTransit Project

---
+++ AC Transit Build/ACTransit076_Data/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,96 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID BUS_REV = 4180996447U;
+        static const AkUniqueID BUS_SLOWINGDOWN = 628700749U;
+        static const AkUniqueID BUTTON_ONCLICK_01 = 3820634141U;
+        static const AkUniqueID BUTTON_ONCLICK_02 = 3820634142U;
+        static const AkUniqueID CANNON = 2393348022U;
+        static const AkUniqueID CITY_AMBIENCE = 3276472951U;
+        static const AkUniqueID CONFIRM_GAMESTART = 1097423108U;
+        static const AkUniqueID MUSIC_START = 3725903807U;
+        static const AkUniqueID PAUSE = 3092587493U;
+        static const AkUniqueID UNHAPPY = 3424595418U;
+        static const AkUniqueID UNPAUSE = 3412868374U;
+        static const AkUniqueID VACUUM_START = 322689977U;
+        static const AkUniqueID VACUUM_STOP = 3750523667U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace MUSIC
+        {
+            static const AkUniqueID GROUP = 3991942870U;
+
+            namespace STATE
+            {
+                static const AkUniqueID BACKGROUND = 3650723969U;
+                static const AkUniqueID LOADING = 3573931707U;
+                static const AkUniqueID MAIN_MENU = 2005704188U;
+                static const AkUniqueID MAP = 1048449605U;
+            } // namespace STATE
+        } // namespace MUSIC
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace MUSIC_SELECTION
+        {
+            static const AkUniqueID GROUP = 3387854417U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID MUSIC = 3991942870U;
+            } // namespace SWITCH
+        } // namespace MUSIC_SELECTION
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID CITY_AMB = 4101389913U;
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID AC_TRANS_GAME = 4034780523U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID BUS = 714721605U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
+        static const AkUniqueID UI = 1551306167U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
